907c40418d8920ec9aa82d94e59ae52128aa9f60
nicolas-ea <nicolas@minimo.io>
2020-07-27 15:52:44 -0300

    Simpler boilerplate.

--- bibliopath-rest-api.php
+++ bibliopath-rest-api.php
@@ -1,17 +1,6 @@
 <?php
 
 /**
- * The plugin bootstrap file
- *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              http://minimo.io/en/nicolas
- * @since             1.0.0
- * @package           Bibliopath_Rest_Api
- *
  * @wordpress-plugin
  * Plugin Name:       Bibliopath REST API
  * Plugin URI:        https://minimo.io/en/2020/bibliopath/
@@ -36,47 +25,3 @@ if ( ! defined( 'WPINC' ) ) {
  * Rename this for your plugin and update it as you release new versions.
  */
 define( 'BIBLIOPATH_REST_API_VERSION', '1.0.0' );
-
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-bibliopath-rest-api-activator.php
- */
-function activate_bibliopath_rest_api() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-bibliopath-rest-api-activator.php';
-	Bibliopath_Rest_Api_Activator::activate();
-}
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-bibliopath-rest-api-deactivator.php
- */
-function deactivate_bibliopath_rest_api() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-bibliopath-rest-api-deactivator.php';
-	Bibliopath_Rest_Api_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, 'activate_bibliopath_rest_api' );
-register_deactivation_hook( __FILE__, 'deactivate_bibliopath_rest_api' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-bibliopath-rest-api.php';
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_bibliopath_rest_api() {
-
-	$plugin = new Bibliopath_Rest_Api();
-	$plugin->run();
-
-}
-run_bibliopath_rest_api();
