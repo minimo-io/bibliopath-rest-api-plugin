1213dba59a7744dc38a51d7c75583ca50337edb9
nicolas-ea <nicolas@minimo.io>
2020-07-27 16:23:37 -0300

    Rest api for ACF.

--- bibliopath-rest-api.php
+++ bibliopath-rest-api.php
@@ -26,21 +26,9 @@ if ( ! defined( 'WPINC' ) ) {
  */
 define( 'BIBLIOPATH_REST_API_VERSION', '1.0.1' );
 
-function bpath_custom_avatar_field( $user ) { ?>
-	<h3>Custom Avatar</h3>
+// Enable the option show in rest
+add_filter( 'acf/rest_api/field_settings/show_in_rest', '__return_true' );
 
-	<table>
-	<tr>
-	<th><label for="be_custom_avatar">Custom Avatar URL:</label></th>
-	<td>
-	<input type="text" name="be_custom_avatar" id="be_custom_avatar" value="<?php echo esc_url_raw( get_the_author_meta( 'be_custom_avatar', $user->ID ) ); ?>" /><br />
-	<span>Type in the URL of the image you'd like to use as your avatar. This will override your default Gravatar, or show up if you don't have a Gravatar. <br /><strong>Image should be 70x70 pixels.</strong></span>
-	</td>
-	</tr>
-	</table>
-	<?php
-}
-add_action( 'show_user_profile', 'bpath_custom_avatar_field' );
-add_action( 'edit_user_profile', 'bpath_custom_avatar_field' );
-
+// Enable the option edit in rest
+add_filter( 'acf/rest_api/field_settings/edit_in_rest', '__return_true' );
 ?>
